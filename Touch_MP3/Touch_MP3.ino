69b646a494ac18c3530382808f46603243b32705
Stefan Dzisiewski-Smith <stefan.dzisiewski.smith@gmail.com>
2013-10-25 17:51:04 +0100

    Updated to use new MPR121_t::getNumTouches()

--- Touch_MP3/Touch_MP3.ino
+++ Touch_MP3/Touch_MP3.ino
@@ -23,21 +23,10 @@
 #include <SdFatUtil.h> 
 #include <SFEMP3Shield.h>
 
-
-// Below is not needed if interrupt driven. Safe to remove if not using.
-//#if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_Timer1
-//  #include <TimerOne.h>
-//#elif defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer
-//  #include <SimpleTimer.h>
-//#endif
-
-
 // mp3 variables
 SFEMP3Shield MP3player;
-byte temp;
 byte result;
 int lastPlayed = 0;
-SdFat sd; 
 
 // touch behaviour definitions
 #define firstPin 0
@@ -47,15 +36,12 @@ SdFat sd;
 void setup(){  
   Serial.begin(57600);
    
-//  while (!Serial) ; {} //uncomment when not using the serial monitor
- 
+  //while (!Serial) ; {} //uncomment when not using the serial monitor 
   Serial.println("Bare Conductive Touch MP3 player");
 
   if(!MPR121.begin(MPR121_ADDR)) Serial.println("error setting up MPR121");
   MPR121.setInterruptPin(MPR121_INT);
-  
-  sd.begin(); //open up communication with the SD card 
-  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
+
   
   result = MP3player.begin();
   MP3player.SetVolume(40,40);
@@ -68,15 +54,6 @@ void setup(){
 }
 
 void loop(){
-  
-//  // Below is only needed if not interrupt driven. Safe to remove if not using.
-//#if defined(USE_MP3_REFILL_MEANS) \
-//    && ( (USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer) \
-//    ||   (USE_MP3_REFILL_MEANS == USE_MP3_Polled)      )
-//
-//  MP3player.available();
-//#endif
-
   readTouchInputs();
 }
 
@@ -85,19 +62,11 @@ void readTouchInputs(){
   if(MPR121.touchStatusChanged()){
     
     MPR121.updateTouchData();
-    
-    int touchedCount = 0;
-    
-    // count number of touched inputs
-    for (int i=0; i<12; i++){
-      if(MPR121.getTouchData(i)){
-        touchedCount++;  
-      }  
-    }
 
     // only make an action if we have one or fewer pins touched
     // ignore multiple touches
-    if(touchedCount<=1){
+    
+    if(MPR121.getNumTouches()<=1){
       for (int i=0; i < 12; i++){  // Check which electrodes were pressed
         if(MPR121.isNewTouch(i)){
         
