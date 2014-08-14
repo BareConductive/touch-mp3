7c9a9b6bb17b4cb32a816ab24b8179c259294728
Stefan Dzisiewski-Smith <stefan.dzisiewski.smith@gmail.com>
2014-08-14 12:31:06 +0100

    Amended licensing details in code header

--- Touch_MP3/Touch_MP3.ino
+++ Touch_MP3/Touch_MP3.ino
@@ -1,15 +1,28 @@
-/**********************************************************
 
-  Bare Conductive Touch MP3 player
-  
- **********************************************************  
-  
-  Code is by Stefan Dzisiewski-Smith, building on example
-  code from Sparkfun Website and Jim Lindblom
+/*******************************************************************************
 
-  Extra modifications by Peter Krige (PK2_modified)
+ Bare Conductive Touch MP3 player
+ ------------------------------
+ 
+ Touch_MP3.ino - touch triggered MP3 playback
+ 
+ Based on code by Jim Lindblom and plenty of inspiration from the Freescale 
+ Semiconductor datasheets and application notes.
+ 
+ Bare Conductive code written by Stefan Dzisiewski-Smith and Peter Krige.
+ 
+ This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 
+ Unported License (CC BY-SA 3.0) http://creativecommons.org/licenses/by-sa/3.0/
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
 
-***********************************************************/
+*******************************************************************************/
 
 // touch includes
 #include <MPR121.h>
